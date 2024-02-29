28d12c397ef0b2b6272b152098e6e354a8078020
emijsho <145356364+emijsho@users.noreply.github.com>
2024-02-29 13:28:43 +0000

    amended files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Weather from "./Weather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Weather />
+      <smaller>
+        <a href="https://github.com/emijsho/sc-react-weather">
+          Open Source Code
+        </a>{" "}
+        by Emily Short. Hosted on{" "}
+        <a href="https://react-weather-ejs.netlify.app/">Netlify.</a>
+      </smaller>
+    </div>
+  );
+}
