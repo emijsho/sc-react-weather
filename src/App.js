970baf090a96e145c40229fc8306720aa79ab269
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-03 18:03:08 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Weather />
-      <smaller>
-        <a href="https://github.com/emijsho/sc-react-weather">
-          Open Source Code
-        </a>{" "}
-        by Emily Short. Hosted on{" "}
-        <a href="https://react-weather-ejs.netlify.app/">Netlify.</a>
-      </smaller>
+      <footer>
+        <smaller>
+          <a href="https://github.com/emijsho/sc-react-weather">
+            Open Source Code
+          </a>{" "}
+          by Emily Short. Hosted on{" "}
+          <a href="https://react-weather-ejs.netlify.app/">Netlify.</a>
+        </smaller>
+      </footer>
     </div>
   );
 }
