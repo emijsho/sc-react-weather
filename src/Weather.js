19b6a66934fd73fbaf526e8af28bbb58f19daee7
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-05 13:29:17 +0000

    updated HTML on Weather.JS

--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,38 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
-export default function SearchEngine() {
-  const [city, setCity] = useState("");
-  const [temperature, setTemperature] = useState();
-  const [description, setDescription] = useState();
-  const [humidity, setHumidity] = useState();
-  const [wind, setWind] = useState();
-  const [weatherIcon, setWeatherIcon] = useState();
-
-  function displayWeather(response) {
-    setTemperature(`${Math.round(response.data.main.temp)}`);
-    setDescription(`${response.data.weather[0].description}`);
-    setHumidity(`${response.data.main.humidity}`);
-    setWind(`${Math.round(response.data.wind.speed)}`);
-    setWeatherIcon(
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=281450ec88936f4fa8ee9864682b49a0&units=metric`;
-    axios.get(url).then(displayWeather);
-  }
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+export default function Weather() {
   return (
-    <div className="Search">
-      <form onSubmit={handleSubmit}>
-        <input type="search" placeholder="Enter city" onChange={updateCity} />
-        <input type="submit" value="Search" />
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <input type="submit" value="Search" classname="btn btn-primary" />
+        </div>
       </form>
-      <p>{city}</p>
+      <h1>New York</h1>
       <ul>
-        <li>Temperature: {temperature}°C </li>
-        <li>Description: {description}</li>
-        <li>Humidity: {humidity}% </li>
-        <li>Wind: {wind}MPH </li>
-        <li>
-          <img src={weatherIcon} alt="weatherIcon" />
-        </li>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
       </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="" alt="Mostly Cloudy" />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
