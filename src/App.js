12d820de7b481caacc606cb621bc95aac6845d73
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-07 14:20:04 +0000

    added API Intergration

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
-      <Weather />
+      <Weather defaultCity="New York" />
       <footer>
-        <smaller>
-          <a href="https://github.com/emijsho/sc-react-weather">
-            Open Source Code
-          </a>{" "}
-          by Emily Short. Hosted on{" "}
-          <a href="https://react-weather-ejs.netlify.app/">Netlify.</a>
-        </smaller>
+        <a href="https://github.com/emijsho/sc-react-weather">
+          Open Source Code
+        </a>{" "}
+        by Emily Short. Hosted on{" "}
+        <a href="https://react-weather-ejs.netlify.app/">Netlify.</a>
       </footer>
     </div>
   );
