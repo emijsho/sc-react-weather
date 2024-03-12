524e4cd32606121d34a57b0ab64def287149a413
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-12 13:15:15 +0000

    added forecast api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,28 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "494f3181eb1oe9bfae0t4f2214913d5b";
+  let latitude = props.coordinates.latitude;
+  let longitude = props.coordinates.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&untis=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecastDay">Thu</div>{" "}
           <div className="WeatherForecastIcon" size="32">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+              alt="description"
+            />
           </div>
           <span className="WeatherForecastHigh">19°C</span>
           <span className="WeatherForecastLow">10°C</span>
