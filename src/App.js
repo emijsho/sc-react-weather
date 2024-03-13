d2c1e423ac44de5a1fedea7b753e4d37e3daee93
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-13 11:30:11 +0000

    added finishing touches

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ export default function App() {
       <h1>Weather Search Engine</h1>
       <Weather defaultCity="Boston" />
       <footer>
-        <a href="https://github.com/emijsho/sc-react-weather">
+        <a
+          href="https://github.com/emijsho/sc-react-weather"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Open Source Code
         </a>{" "}
         by Emily Short. Hosted on{" "}
-        <a href="https://react-weather-ejs.netlify.app/">Netlify.</a>
+        <a
+          href="https://react-weather-ejs.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify.
+        </a>
       </footer>
     </div>
   );
