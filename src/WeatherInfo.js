7dd402168f6d1ea52a7a36df0e20c09fdb6240e7
emijsho <145356364+emijsho@users.noreply.github.com>
2024-03-13 11:59:14 +0000

    amended finishing touches css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,12 +4,13 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+      <h1>
+        <strong>{props.data.city}</strong>
+      </h1>
       <ul>
         <li>
           <FormatDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
@@ -21,8 +22,13 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.wind)} MPH</li>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li>
+              Humidity: <strong>{props.data.humidity}%</strong>
+            </li>
+            <li>
+              Wind: <strong>{Math.round(props.data.wind)} MPH</strong>
+            </li>
           </ul>
         </div>
       </div>
